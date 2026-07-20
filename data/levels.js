fe16fa92c34d54ae3fe7c2a44bec531f712d73fd
Mara Lurkin <mara.lurkin@hotmail.fr>
2026-07-20 12:53:14 +0200

    blabla

--- data/levels.js
+++ data/levels.js
@@ -153,8 +153,10 @@ window.BANQUE_NIVEAUX = [
 
   {
     titre: "Kata (Code Wars)",
+
     texte:
-      "Le Kata (Code Wars) définit les niveaux de difficulté des exercices d'entraînement à l'algorithmique. Très pratiqués par les membres de cette formation, ils visent à s'entraîner dur dans le but de réussir l'examen final de préformation. La réussite d'un niveau 5 procure un sentiment de bonheur intense et de réassurance personnelle nécessaire avant d'enchaîner sans crainte les niveau 7.",
+      "Le Kata (Code Wars) définit le niveau de difficulté des exercices d'entraînement à l'algorithmique. Très pratiqués par les membres de cette formation, il vise à s'entraîner dur dans le but de réussir l'examen final de préformation. La réussite d'un niveau 5 procure un sentiment de bonheur intense et de réassurance personnelle nécessaire avant d'enchaîner sans crainte les niveaux 7.",
+
     indices: [
       "difficulte",
       "exercices",
@@ -164,6 +166,7 @@ window.BANQUE_NIVEAUX = [
       "bonheur",
       "reassurance",
     ],
+
     variantes: {
       "kata": ["kata", "katas"],
       "code": ["coder", "code"],
@@ -187,5 +190,93 @@ window.BANQUE_NIVEAUX = [
       "bonheur": ["bonheur", "joie", "content", "heureux"],
       "reassurance": ["reassurance", "confiance", "rassurer", "personnelle"],
     },
+
+    similarities : {
+      "kata": ["exercice", "challenge", "entrainement", "codewars"],
+      "code": ["coder", "programmer", "developper"],
+      "difficulte": ["challenge", "complexite", "niveau", "hard"],
+      "exercices": ["entrainement", "pratique", "challenge"],
+      "niveau": ["level", "rank", "grade"],
+      "algorithmique": ["algorithme", "programmation", "codage"],
+      "formation": ["cours", "apprentissage", "robin"],
+      "bonheur": ["joie", "content", "satisfaction"],
+      "reassurance": ["confiance", "rassurer", "soutien"],
+    }
   },
+
+  {
+    titre : "Odoo",
+
+    texte : "Odoo (anciennement « Tiny ERP » puis « OpenERP») est un logiciel de gestion d'affaires open source créé par le Belge Fabien Pinckaers, fondateur et CEO de la société Odoo. À l’origine développé pour être un progiciel de gestion intégré (ERP), le logiciel s’est vu étendre ses fonctionnalités à d'autres domaines et est devenu une suite applicative comprenant une multitude de modules métiers, appelés apps : gestion de la relation client (CRM), gestion de contenu (CMS), e-commerce, blogs, forums, news, événements, live chat, offres d'emploi, etc. Le logiciel est utilisé par plus de quinze millions d’utilisateurs pour gérer leurs entreprises à travers le monde. Odoo est décliné en deux versions : community, disponible gratuitement sous la licence LGPLv3, et entreprise, disponible sous la licence propriétaire Odoo Enterprise Edition License v1.0.",
+
+    indices : ["logiciel", "gestion", "open source", "ERP", "apps", "entreprise"],
+
+    variantes : {
+      "anciennement" : ["ancien", "anciennement"],
+      "est" : ["etre", "est"],
+      "de" : ["de", "du"],
+      "d": ["de", "du"],
+      "créé": ["creer", "créé"],
+      "la" : ["le", "la"],
+      "l": ["le", "la"],
+      "développé" : ["developper", "développé"],
+      "intégré" : ["integrer", "intégré"],
+      "s" : ["se"],
+      "vu" : ["voir", "vu"],
+      "ses" : ["son", "ses"],
+      "fonctionnalités" : ["fonctionnalité", "fonction", 'fonctions'],
+      "autres": ["autre"],
+      "domaines": ["domaine", "domaines"],
+      "une" : ["un", "une"],
+      "comprenant": ["comprendre", "comprenant"],
+      "apps" : ["app", "apps", "application", "applications"],
+      "leurs": ["leur", "leurs"]
+    },
+
+    similarities : {
+      "odoo" : ["erp", "open source", "logiciel", "gestion"],
+      "logiciel" : ["programme", "application", "outil"],
+      "gestion" : ["administration", "organisation", "management"],
+      "belge": ["belgique", "belgian", "belge"]
+    }
+
+  },
+
+  
+
+  {
+    titre : "Inventaire de bites",
+    texte : "Un inventaire de bites (anglais: dick_inventory) est une structure de données qui permet de stocker et gérer efficacement des données. Elle est particulièrement utile pour les accès rapides aux éléments. Découvert grâce à Philippe lors d'un cours d'algorithmique avancé, nous avons découvert que l'utilisation de threads dans cet inventaire permettait d'utiliser plusieurs bites en même temps.",
+
+    indices : ["structure", "données", "threads", "Philippe"],
+
+    variantes : 
+    {
+      "de": ["du"],
+      "est" : ['être'],
+      "une" : ["un"],
+      "qui" : ["que"],
+      "des" : ["de", "du"],
+      "elle" : ["il"],
+      "les" : ["le"],
+      "rapides" : ["rapide"],
+      "aux" : ["à"],
+      "éléments" : ["élément"],
+      "d" : ["de", "du"],
+      "avons" : ["avoir"],
+      "découvert" : ["découvrir"],
+      "l" : ["le"],
+      "permettait" : ["permettre"]
+    },
+
+    similarities :
+    { 
+      "inventaire" : ["groupe", "répertoire", "liste"],
+      "bite" : ["penis", "teub", "vagin", "élément"],
+      "structure" : ["data" , "agencement"],
+      "stocker" : ["enregistrer", "garder", "retenir"],
+      "threads" : ["parallèle", "plusieurs", "multiple"]
+    }
+
+  }
 ];
